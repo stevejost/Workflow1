0024cd6844499a291d94f43274f2fdbb9032aaf5
Steve Jost <steve@stevejost.com>
2015-05-15 09:30:27 -0700

    Initial Commit

---
+++ AwesomeApplication/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace AwesomeApplication
+{
+    using System;
+    using System.IO;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            TestClass c = new TestClass();
+
+            Console.WriteLine(c.TestString);
+            
+        }
+    }
+}
---
+++ AwesomeApplication/TestClass.cs
@@ -0,0 +1,19 @@
+﻿namespace AwesomeApplication
+{
+    /// <summary>
+    /// The best class in the world.
+    /// </summary>
+    public class TestClass
+    {
+        /// <summary>
+        /// Gets the best property in the world.
+        /// </summary>
+        public string TestString 
+        {
+            get
+            {
+                return "The best string in the world.";
+            } 
+        }
+    }
+}
